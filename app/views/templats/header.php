5a995f067a244d3f0252713e39aea98105c75e21
NaufalArdian12 <naufalopo87@gmail.com>
2024-11-01 07:38:08 +0700

    views

---
+++ app/views/templats/header.php
@@ -0,0 +1,49 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <script src="https://cdn.tailwindcss.com"></script>
+    <title>Halaman <?= $data['title']; ?></title>
+    <!-- favicon link -->
+    <link rel="shortcut icon" href="images/favicon.ico" type="image/x-icon">
+    <!-- google font link -->
+    <link href="https://fonts.googleapis.com/css2?family=Inter:wght@100;200;300;400;500;600;700;800;900&display=swap" rel="stylesheet">
+    <!-- tailwind css cdn -->
+    <script src="https://cdn.tailwindcss.com"></script>
+    <!-- ionicons cdn -->
+    <script type="module" src="https://unpkg.com/ionicons@5.5.2/dist/ionicons/ionicons.esm.js"></script>
+    <!-- alpine js cdn -->
+    <script defer src="https://unpkg.com/alpinejs@3.x.x/dist/cdn.min.js"></script>
+    <!-- style css -->
+    <style type="text/tailwindcss">
+        body{
+            font-family: 'Inter', sans-serif;
+            
+        }
+
+        html {
+            scroll-behavior: smooth;
+        }
+
+        body {
+            overflow: auto; /* Ini untuk memastikan body dapat di-scroll */
+        }
+
+        ::-webkit-scrollbar {
+            display: none; /* Sembunyikan scrollbar di browser berbasis WebKit */
+        }
+    </style>
+    <script>
+        document.documentElement.classList.add('js')
+    </script>
+
+</head>
+
+<body>
+
+
+    </head>
+
+    <body>
