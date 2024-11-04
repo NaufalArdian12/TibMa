359800f6bf2a06db6e6ef18202f325a787606a7f
NaufalArdian12 <naufalopo87@gmail.com>
2024-11-04 17:04:58 +0700

    add config

---
+++ app/config/routes.php
@@ -0,0 +1,9 @@
+<?php
+
+return [
+  'login' => ['controller' => 'AuthController', 'method' => 'index'],
+  'register' => ['controller' => 'AuthController', 'method' => 'register'],
+  'authenticate' => ['controller' => 'AuthController', 'method' => 'authenticate'],
+  'logout' => ['controller' => 'AuthController', 'method' => 'logout'],
+];
+
