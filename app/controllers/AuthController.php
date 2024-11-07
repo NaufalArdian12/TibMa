0cbce891cb7055e5b7fc00d8ea26ebfbaba95822
NaufalArdian12 <naufalopo87@gmail.com>
2024-11-07 14:45:34 +0700

    little bit changes on view index

--- app/controllers/AuthController.php
+++ app/controllers/AuthController.php
@@ -18,19 +18,18 @@ class AuthController extends Controller
             // Ambil data input dari form login
             $username = $_POST['username'];
             $password = $_POST['password'];
-            
+
             // Muat model user untuk verifikasi
             $userModel = $this->model('user\UserModels');
             $user = $userModel->findUserByUsername($username);
             $authenticated = $userModel->verifyPassword($username, $password);
-var_dump($authenticated);
+            var_dump($authenticated);
             // Periksa apakah user ditemukan dan password valid
             if ($authenticated) {
                 // Jika valid, simpan session dan tampilkan pesan login berhasil
                 session_start();
                 $_SESSION['user'] = $user['username'];
                 echo "Login berhasil! Selamat datang, " . $_SESSION['user'];
-                
             } else {
                 // Jika tidak valid, tampilkan pesan error
                 $data = ['title' => 'Login', 'error' => 'Username atau password salah.'];
@@ -38,7 +37,6 @@ var_dump($authenticated);
                 $this->view('auth/login', $data);
                 $this->view('templats/footer');
             }
-            
         } else {
             // Jika metode bukan POST, alihkan ke halaman login
             header("Location: /login");
@@ -47,8 +45,9 @@ var_dump($authenticated);
     }
 
 
-    function register(){
-        if($_SERVER['REQUEST_METHOD'] == 'POST'){
+    function register()
+    {
+        if ($_SERVER['REQUEST_METHOD'] == 'POST') {
             $salt = bin2hex(random_bytes(8));
             $password_hash = hash('sha256', $_POST['password'] . $salt);
             $userModel = $this->model('user\UserModels');
@@ -61,7 +60,7 @@ var_dump($authenticated);
             $userModel->save();
         }
         $data = ['title' => 'Register'];
-        
+
         $this->view('templats/header', $data);
         $this->view('auth/register');
         $this->view('templats/footer');
