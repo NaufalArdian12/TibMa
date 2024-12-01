44a0b3418b8a2f1f727dbdbbf7721bc8e2a4529b
NaufalArdian12 <naufalopo87@gmail.com>
2024-12-01 17:37:24 +0700

    config

---
+++ config.php
@@ -0,0 +1,51 @@
+<?php
+/*
+   |--------------------------------------------------------------------------------------
+   | Database Configuration
+   |--------------------------------------------------------------------------------------
+   |
+   | Here you can configure your application database and its settings.
+   |
+   | 'name'        => 	string 		The name of the database.
+   | 'host'        => 	string 		The database host.
+   | 'username'	=> 	string 		The database username.
+   | 'password'    => 	string 		The database password.
+   | 'options '	=> 	array 		An array of extra attributes on the database handle.
+   |                         		Refer to http://php.net/manual/en/pdo.setattribute.php.
+   |
+   */
+$dotenv = Dotenv\Dotenv::createImmutable(__DIR__);
+$dotenv->load();
+
+return [
+  'database' => [
+    'name' => $_ENV['DB_NAME'],
+    'host' => $_ENV['DB_HOST'],
+    'port' => $_ENV['DB_PORT'],
+    'username' => $_ENV['DB_USER'],
+    'password' => $_ENV['DB_PASSWORD'],
+    'options' => array(
+      // PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
+      // PDO::MYSQL_ATTR_SSL_VERIFY_SERVER_CERT => false,
+      // PDO::MYSQL_ATTR_SSL_CA => "../../../database_ca.pem",
+      // PDO::MYSQL_ATTR_INIT_COMMAND => "SET NAMES utf8",
+    ),
+    'pagination_limit' => 15,
+  ],
+  'host' => $_ENV['HOST'],
+  'root_uri' => "http://tibma.test",
+  'public_uri' => "http://tibma.test/public",
+  'gmail' => [
+    'sender_name' => $_ENV['GMAIL_SENDER_NAME'],
+    'sender_email' => $_ENV['GMAIL_SENDER_EMAIL'],
+    'app_password' => $_ENV['GMAIL_APP_PASSWORD'],
+  ],
+  'timezone' => 'Asia/Jakarta',
+  'recovery_token_validity' => 60 * 60, // in seconds
+  'password_hash_algorithm' => PASSWORD_BCRYPT,
+  'cloudinary' => [
+    'connection_url' => $_ENV['CLOUDINARY_CONNECTION_URL'],
+    'access_url' => $_ENV['CLOUDINARY_ACCESS_URL'],
+    'max_image_size' => 1024 * 1024 * 5, // 5 MB
+  ]
+];
---
+++ routes.php
@@ -0,0 +1,18 @@
+<?php
+
+/*
+   |--------------------------------------------------------------------------
+   | Web Routes
+   |--------------------------------------------------------------------------
+   |
+   | Here is where you can register web routes for your application. These
+   | routes are loaded by the Router class 
+   |
+   */
+
+require 'app/routes/global_routes.php';
+require 'app/routes/auth_routes.php';
+require 'app/routes/admin_routes.php';
+require 'app/routes/dosen_routes.php';
+require 'app/routes/mahasiswa_routes.php';
+require 'app/routes/report_routes.php';
